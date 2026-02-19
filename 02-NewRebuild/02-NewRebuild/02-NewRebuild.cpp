19ccf61060142c3da2fcc998a580595ec1341917
aelaina100 <aelaina100@gmail.com>
2026-02-18 21:53:40 -0500

    Automating new functionalities: 02-NewRebuild && 022NewNew

---
+++ 02-NewRebuild/02-NewRebuild/02-NewRebuild.cpp
@@ -0,0 +1,15 @@
+// 02-NewRebuild.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+#include <iostream>
+
+int main()
+{
+  // I am the best QA.
+  // Becasue I bring dedication, consciouncessness and discipline;
+  // and being adamant about learning new strategies, technologies, and tools
+
+	// I will also become the BEST DEVELOPER in my next position.
+}
+
+
